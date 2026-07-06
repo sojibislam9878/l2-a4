929c7c918e82515de7d348b4432c1686b9a5af3a
sojibhasan9878 <sojibhasan9878@gmail.com>
2026-07-06 15:24:00 +0600

    stripe key and npm packes added

--- src/config/envConfiq.ts
+++ src/config/envConfiq.ts
@@ -10,6 +10,8 @@ const envConfig ={
     jwt_refresh_secret: process.env.JWT_REFRESH_SECRET!,
     jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN!,
     jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN!,
+    payment_publishable_key: process.env.PAYMENT_PUBLISHABLE_KEY!,
+    payment_secret_key: process.env.PAYMENT_PUBLISHABLE_KEY!,
     app_url: process.env.JWT_REFRESH_EXPIRES_IN!,
 
 }
